90c3c2ab6c0d70e9caaffd9eccf9d45750ee864c
Nikita Zabaykin <vladlihovid@gmail.com>
2017-12-03 00:41:35 +0200

    Implimented index page

--- src/main/java/controller/TestServlet.java
+++ src/main/java/controller/TestServlet.java
@@ -1,11 +1,18 @@
 package controller;
 
+import model.data.Database;
+import model.entity.Necklace;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import static util.Constants.NECKLACES_LIST_ATTRIBUTE_KEY;
+import static util.Constants.NECKLACE_NAME_REQUEST_PARAMETER;
+import static util.Constants.STONES_LIST_ATTRIBUTE_KEY;
 
 /**
  * controller.TestServlet
@@ -14,10 +21,20 @@ import java.io.IOException;
  * @author Nikita Zabaykin vladlihovid@gmail.com
  * @version 1.0
  */
-@WebServlet
+@WebServlet(urlPatterns = "/")
 public class TestServlet extends HttpServlet {
+    private Database database = Database.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Kuk");
+        req.setAttribute(STONES_LIST_ATTRIBUTE_KEY, database.getStones());
+        req.setAttribute(NECKLACES_LIST_ATTRIBUTE_KEY, database.getNecklaces());
+        getServletContext().getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        database.getNecklaces().add(new Necklace(((String) req.getParameter(NECKLACE_NAME_REQUEST_PARAMETER))));
+        resp.sendRedirect("/");
     }
 }
