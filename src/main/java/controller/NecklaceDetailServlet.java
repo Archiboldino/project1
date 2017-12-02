46764e84e1117704ac5112119d77f60cffbbdaa1
Nikita Zabaykin <vladlihovid@gmail.com>
2017-12-03 00:40:57 +0200

    Added tomcat plugin

---
+++ src/main/java/controller/NecklaceDetailServlet.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * NecklaceDetailServlet
+ * created on 03.12.2017
+ *
+ * @author Nikita Zabaykin vladlihovid@gmail.com
+ * @version 1.0
+ */
+public class NecklaceDetailServlet {
+}
