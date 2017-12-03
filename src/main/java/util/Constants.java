e68c608c211208e85c5996bee373915d692b9e6d
Nikita Zabaykin <vladlihovid@gmail.com>
2017-12-03 11:54:15 +0200

    Refactored constatnts to separate interface

---
+++ src/main/java/util/Constants.java
@@ -0,0 +1,15 @@
+package util;
+
+/**
+ * Constants
+ * created on 02.12.2017
+ *
+ * @author Nikita Zabaykin vladlihovid@gmail.com
+ * @version 1.0
+ */
+public interface Constants {
+    String STONES_LIST_ATTRIBUTE_KEY = "stones";
+    String NECKLACES_LIST_ATTRIBUTE_KEY = "necklaces";
+    String NECKLACE_NAME_REQUEST_PARAMETER = "necklaceName";
+    String NECKLACE_ATTRIBUTE_KEY = "necklace";
+}
