4c28a75418685e56a292aa95084f51b79be185b2
Nikita Zabaykin <vladlihovid@gmail.com>
2017-12-03 16:34:18 +0200

    Added internat constants

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -12,4 +12,20 @@ public interface Constants {
     String NECKLACES_LIST_ATTRIBUTE_KEY = "necklaces";
     String NECKLACE_NAME_REQUEST_PARAMETER = "necklaceName";
     String NECKLACE_ATTRIBUTE_KEY = "necklace";
+    String STONE_ID_REQUEST_PARAMETER = "stoneId";
+    String NECKLACE_ID_REQUEST_PARAMETER = "id";
+    String BUNDLE_ATTRIBUTE_KEY = "bundle";
+    String INDEX_BUNDLE_PATH = "index_strings";
+    String DETAIL_BUNDLE_PATH = "necklace_detail_strings";
+
+    String BUNDLE_INDEX_NECKLACES_TITLE = "index.necklaces.title";
+    String BUNDLE_INDEX_STONES_TITLE = "index.stones.title";
+    String BUNDLE_INDEX_NECKLACE_INPUT_PLACEHOLDER = "index.necklaces.form.tftooltip";
+    String BUNDLE_INDEX_NECKLACE_ADDBUTTON = "index.necklaces.form.buttontext";
+
+    String BUNDLE_DETAIL_STONES_TITLE_KEY = "detail.stones.title";
+    String BUNDLE_DETAIL_WEIGHT = "detail.stones.weight";
+    String BUNDLE_DeTAIL_ALLSTONES_TITLE = "detail.allstones.title";
+
+    String NECKLACE_DETAIL_URL = "/necklace?id=";
 }
