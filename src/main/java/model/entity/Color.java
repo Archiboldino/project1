70927bd17bc80d484dfa665054ef2df110057343
Nikita Zabaykin <vladlihovid@gmail.com>
2017-12-07 19:56:23 +0200

    Renamed to Color

--- src/main/java/model/entity/Color.java
+++ src/main/java/model/entity/Color.java
@@ -1,25 +1,21 @@
 package model.entity;
 
 /**
- * Colour
+ * Color
  * created on 07.12.2017
  *
  * @author Nikita Zabaykin vladlihovid@gmail.com
  * @version 1.0
  */
-public enum Colour {
+public enum Color {
     RED("#ff0000"), GREEN("#00ff00"), BLUE("#0000ff");
     private String colourHex;
 
-    Colour(String colourHex) {
+    Color(String colourHex) {
         this.colourHex = colourHex;
     }
 
     public String getColourHex() {
         return colourHex;
-    }
-
-    public void setColourHex(String colourHex) {
-        this.colourHex = colourHex;
     }
 }
