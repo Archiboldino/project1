e862bfc71b0d6fa19335fe3aeb92e791acf947f4
Nikita Zabaykin <vladlihovid@gmail.com>
2017-12-09 23:27:19 +0200

    Implemented jdbc stone dao

---
+++ src/main/java/model/data/dao/impl/JDBCPreciousStoneDao.java
@@ -0,0 +1,103 @@
+package model.data.dao.impl;
+
+import model.data.dao.PreciousStoneDao;
+import model.data.dao.util.PreciousStoneMaper;
+import model.entity.PreciousStone;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * JDBCPreciousStoneDao
+ * created on 09.12.2017
+ *
+ * @author Nikita Zabaykin vladlihovid@gmail.com
+ * @version 1.0
+ */
+public class JDBCPreciousStoneDao implements PreciousStoneDao {
+    private Connection connection;
+
+    public JDBCPreciousStoneDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public List<PreciousStone> getAll() {
+        List<PreciousStone> res = new ArrayList<>();
+        try (Statement st = connection.createStatement()) {
+            ResultSet rs = st.executeQuery("SELECT id, name, price, color, transparency FROM precious_stone");
+
+            while (rs.next()) {
+                res.add(PreciousStoneMaper.mapFromResultSet(rs));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return res;
+    }
+
+    @Override
+    public PreciousStone getById(int id) {
+        try (PreparedStatement st = connection.
+                prepareStatement("SELECT id, name, price, color, transparency FROM precious_stone " +
+                        "WHERE id=?;")) {
+            st.setInt(1, id);
+
+            ResultSet rs = st.executeQuery();
+
+            rs.next();
+            return PreciousStoneMaper.mapFromResultSet(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void insert(PreciousStone item) {
+        try (PreparedStatement st = connection.
+                prepareStatement("INSERT INTO  precious_stone (name, price, color, transparency, weight) " +
+                        "VALUES (?, ?, ?, ?, ?)")) {
+            st.setString(1, item.getName());
+            st.setLong(2, item.getPrice());
+            st.setInt(3, item.getColor().ordinal());
+            st.setDouble(4, item.getTransparency());
+            st.setDouble(5, item.getWeight());
+
+            st.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete(PreciousStone item) {
+        try (PreparedStatement st = connection.
+                prepareStatement("DELETE FROM  precious_stone " +
+                        "WHERE id=?")) {
+            st.setInt(1, item.getId());
+
+            st.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void update(PreciousStone item) {
+        try (PreparedStatement st = connection.
+                prepareStatement("UPDATE  precious_stone " +
+                        " SET name=?, color=?, transparency=?, price=?, weight=? WHERE id=?")) {
+            st.setString(1, item.getName());
+            st.setDouble(2, item.getTransparency());
+            st.setLong(3, item.getPrice());
+            st.setDouble(4, item.getWeight());
+            st.setInt(4, item.getId());
+
+            st.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/model/data/dao/util/PreciousStoneMaper.java
+++ src/main/java/model/data/dao/util/PreciousStoneMaper.java
@@ -1,5 +1,11 @@
 package model.data.dao.util;
 
+import model.entity.Color;
+import model.entity.PreciousStone;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * PreciousStoneMaper
  * created on 09.12.2017
@@ -8,4 +14,20 @@ package model.data.dao.util;
  * @version 1.0
  */
 public class PreciousStoneMaper {
+    public static PreciousStone mapFromResultSet(ResultSet rs) {
+        try {
+            PreciousStone stone = new PreciousStone.Builder()
+                    .id(rs.getInt("id"))
+                    .name(rs.getString("name"))
+                    .color(Color.values()[rs.getInt("color")])
+                    .transparency(rs.getDouble("transparency"))
+                    .price(rs.getLong("price"))
+                    .weight(rs.getDouble("weight")).build();
+
+            return stone;
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
