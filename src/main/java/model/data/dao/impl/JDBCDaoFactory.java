37e65a4b98325f64704596b6205ac8f55f6a1383
Nikita Zabaykin <vladlihovid@gmail.com>
2017-12-09 23:26:50 +0200

    Implemented jdbc dao factory

--- src/main/java/model/data/dao/impl/JDBCDaoFactory.java
+++ src/main/java/model/data/dao/impl/JDBCDaoFactory.java
@@ -1,5 +1,15 @@
 package model.data.dao.impl;
 
+import model.data.dao.DaoFactory;
+import model.data.dao.NecklaceDao;
+import model.data.dao.PreciousStoneDao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static util.Constants.*;
+
 /**
  * JDBCDaoFactory
  * created on 09.12.2017
@@ -7,5 +17,22 @@ package model.data.dao.impl;
  * @author Nikita Zabaykin vladlihovid@gmail.com
  * @version 1.0
  */
-public class JDBCDaoFactory {
+public class JDBCDaoFactory extends DaoFactory {
+    @Override
+    public PreciousStoneDao createStoneDao() {
+        return new JDBCPreciousStoneDao(getConnection());
+    }
+
+    @Override
+    public NecklaceDao createNecklaceDao() {
+        return new JDBCNecklaceDao(getConnection());
+    }
+
+    private Connection getConnection() {
+        try {
+            return DriverManager.getConnection(DATABASE_PATH, DATABASE_USER, DATABASE_PASS);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
---
+++ src/main/java/model/data/dao/util/PreciousStoneMaper.java
@@ -0,0 +1,11 @@
+package model.data.dao.util;
+
+/**
+ * PreciousStoneMaper
+ * created on 09.12.2017
+ *
+ * @author Nikita Zabaykin vladlihovid@gmail.com
+ * @version 1.0
+ */
+public class PreciousStoneMaper {
+}
