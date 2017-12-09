43b9112e8242a3755527f55401b43cb4c33c830c
Nikita Zabaykin <vladlihovid@gmail.com>
2017-12-10 00:37:36 +0200

    Implemented necklace maper

--- src/main/java/model/data/dao/util/NecklaceMaper.java
+++ src/main/java/model/data/dao/util/NecklaceMaper.java
@@ -1,5 +1,12 @@
 package model.data.dao.util;
 
+import model.entity.Necklace;
+import model.entity.PreciousStone;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
 /**
  * NecklaceMaper
  * created on 10.12.2017
@@ -8,4 +15,20 @@ package model.data.dao.util;
  * @version 1.0
  */
 public class NecklaceMaper {
+    public static Necklace mapFromResultSet(ResultSet rs) {
+        try {
+            Necklace necklace = new Necklace(rs.getString("necklace_name"));
+            necklace.setId(rs.getInt("necklace_id"));
+            return necklace;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void makeUniqueStones(Necklace necklace, PreciousStone stone, Map<Integer, PreciousStone> stones) {
+        if (stone.getId() != 0) {
+            stones.putIfAbsent(stone.getId(), stone);
+            necklace.getPreciousStones().add(stones.get(stone.getId()));
+        }
+    }
 }
